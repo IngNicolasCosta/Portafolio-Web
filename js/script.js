6d83de82b9416071486c55263bfc689905eb9001
Nicolas <nicolascosta15112004@gmail.com>
2026-03-14 00:55:20 -0300

    Responsive
    Se modifico gran parte del css para que la pagina se adapte bien al cambio de tamaño de pantalla, se agrego los 3 puntitos clásico para el nav cuando la pantalla es chica

--- js/script.js
+++ js/script.js
@@ -186,4 +186,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
         });
     }
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
 });
